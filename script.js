096eb34d5c96c6f06dd7df0496a3251d5106b3d1
Abhishek91K <abhiKMECH91@gmail.com>
2024-10-22 22:23:01 +0530

    Responsiveness for android
    Used media queries, padding & margin adjustments, width changes and position property.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Playing song using javascript');
 let currentSong = new Audio();
 
 function convertSecondsToMinutes(seconds) {
-    if(isNaN(seconds) || seconds <=0){
+    if (isNaN(seconds) || seconds <= 0) {
         return "Invalid Input";
     }
     const minutes = Math.floor(seconds / 60);
@@ -20,8 +20,8 @@ async function getSongs() {
 
     let a = await fetch("http://127.0.0.1:3000/songs/");
     let response = await a.text();
-    // console.log(response);
 
+    // console.log(response);
     let div = document.createElement("div");
     div.innerHTML = response;
 
@@ -41,28 +41,33 @@ async function getSongs() {
 }
 
 
-const playMusic = (track, pause = false) =>{
+
+const playMusic = (track, pause = false) => {
+
     // let audio = new Audio("/songs/" + track);
+    // console.log(track);
     currentSong.src = "/songs/" + track;
-    if(!pause){
+    if (!pause) {
         currentSong.play();
         play.src = "images/pause.svg";
     }
 
-    document.querySelector(".songInfo").innerHTML = decodeURI(track.split(".mp3")[0]);
+    document.querySelector(".songInfo").innerHTML = decodeURI(track.split(".mp3")[0]);//decode - to remove %20 and other encoding
     document.querySelector(".songTime").innerHTML = "00:00/00:00";
+
 }
 
 
 async function main() {
-    
+
 
     //get the list of all songs
     let songs = await getSongs();
-    playMusic(songs[1],true);
+    playMusic(songs[0], true);
 
     //Show all the songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    console.log(songUL);
     for (const song of songs) {
         songUL.innerHTML += `<li>
             <img class="invert" src="images/music.svg" alt="">
@@ -86,31 +91,41 @@ async function main() {
     });
 
     //Attach an event Listener to play, next and previous
-    play.addEventListener("click",()=>{
-        if(currentSong.paused){
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
             currentSong.play();
             play.src = "images/pause.svg";
 
         }
-        else{
+        else {
             currentSong.pause();
             play.src = "images/play.svg";
         }
     })
 
     //listen for timeupdate event
-    currentSong.addEventListener("timeupdate",(a)=>{
+    currentSong.addEventListener("timeupdate", (a) => {
         console.log(currentSong.currentTime, currentSong.duration);
-        document.querySelector(".songTime").innerHTML = `${convertSecondsToMinutes(currentSong.currentTime)}/${convertSecondsToMinutes(currentSong.duration)}`;
+        document.querySelector(".songTime").innerHTML = `${convertSecondsToMinutes(currentSong.currentTime)} / ${convertSecondsToMinutes(currentSong.duration)}`;
 
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     })
 
     //listen for seekbar
-    document.querySelector(".seekBar").addEventListener("click",e=>{
-        let fraction = (e.offsetX / e.target.getBoundingClientRect().width);
-        document.querySelector(".circle").style.left = fraction*100 + "%";
+    document.querySelector(".seekBar").addEventListener("click", e => {
+        let fraction = (e.offsetX / e.target.getBoundingClientRect().width);//getBoundingClientRect().width : This gets the width of the .seekBar element in pixels.
+        document.querySelector(".circle").style.left = fraction * 100 + "%";
         currentSong.currentTime = currentSong.duration * fraction;
+    })
+
+    //add an event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = 0;
+    })
+
+    //add an event listener for close button
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "-120%";
     })
 }
 
